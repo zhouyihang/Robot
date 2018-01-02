c12fe4e7034cdecb1ac977d9cc416f686ee7ae80
zhou yihang <zhouyihangok@163.com>
2018-01-02 18:37:00 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,9 @@ def hello():
     key ="nike"
     num =0
     fileName=""
-    spider(num,key,fileName)
+    thread = Thread(target = spider(num,key,fileName))
+    thread.start()
+#    spider(num,key,fileName)
     return "Hello World!"
 
 if __name__ == "__main__":
