7cc1d983b841b1dd19541ae1243ddc9342a34ed1
zhou yihang <zhouyihangok@163.com>
2018-01-02 17:22:50 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from taobao import spider
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    key ="nike"
+    num =0
+    fileName=""
+    spider(num,key,fileName)
     return "Hello World!"
 
 if __name__ == "__main__":
